195f53a8b8ebb79a0fd8ae31034ce1a54f6178aa
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-02-24 16:55:32 +0530

    routes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -26,42 +26,6 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
-// DELETE Note
-router.delete("/:id", authMiddleware, async (req, res) => {
-  try {
-    const note = await Note.findById(req.params.id);
-    if (!note) return res.status(404).json({ message: "Note not found" });
-
-    if (note.user.toString() !== req.user.id)
-      return res.status(401).json({ message: "Not authorized" });
-
-    await note.deleteOne();
-    res.json({ message: "Note deleted" });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-// EDIT Note
-router.put("/:id", authMiddleware, async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    let note = await Note.findById(req.params.id);
-    if (!note) return res.status(404).json({ message: "Note not found" });
-
-    if (note.user.toString() !== req.user.id)
-      return res.status(401).json({ message: "Not authorized" });
-
-    note.title = title;
-    note.content = content;
-    const updatedNote = await note.save();
-    res.json(updatedNote);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-
 // Fetch all notes for the logged-in user
 router.get("/", authMiddleware, async (req, res) => {
   try {
